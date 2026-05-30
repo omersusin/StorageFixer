541749aaaa3f74156db0f85e3c0a07cfe7a5c0d4
Soul Spark <spark_soul@mail.ru>
2026-05-30 18:55:23 +0530

    fix: update xposed api dependency
    Replace local JAR files with the official Xposed API dependency from the
    Xposed repository.
    
    Signed-off-by: Soul Spark <spark_soul@mail.ru>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,10 +8,12 @@ pluginManagement {
 
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://jitpack.io") }
+        maven(url = "https://jitpack.io")
+        maven(url = "https://api.xposed.info/")
     }
 }
 
